7f0098cceca1fa2e75245438303a7c4fcbb1875c
Marcel Arie <googlillo@gmail.com>
2023-04-04 16:37:33 +0200

    update script

--- check-single-app-change.sh
+++ check-single-app-change.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
 # ignored_files=( ".codeclimate.yml" ".dockerignore" ".DS_Store" ".eslintignore" ".eslintrc" ".gitattributes" ".gitignore" ".nvmrc" ".yarnrc" "ambient.d.ts" "babel.config.js" "catalog-info.yaml" "Dockerfile" "Dockerfile-sonar" "google_chrome_stable_version_number.txt" "Jenkinsfile" "JenkinsfileLatestChrome" "Makefile" "package.json" "pom.xml" "postcss.config.js" "README.md" "settings.xml" "sonar-project.properties" "stuart_logo.png" "test-report.xml" "tsconfig.json" "yarn-error.log" "yarn.lock")
+apps=$(ls apps)
 
-git diff  origin/master --name-only | grep --quiet "^folder_1/.*"
+for app in $apps; do
+	if git diff origin/master --name-only | grep --quiet "^apps/$app/.*"; then
+		echo "$app has changes"
+	else
+		echo "$app has no changes"
+	fi
+done
